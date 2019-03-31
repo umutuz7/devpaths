2585f765c401e76ff4a4787655052ddf09428e83
Umut Uz <umut.uz@bilyoner.com>
2019-03-31 21:03:21 +0300

    Add put and get message requests

---
+++ src/main/java/hello/message/MessageController.kt
@@ -0,0 +1,24 @@
+package hello.message
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class MessageController {
+
+
+    @PostMapping("/messages")
+    fun putMessage(@RequestBody message: String): String {
+        return MessageHolder.putMessage(message)
+    }
+
+    @GetMapping("/messages/{hash}")
+    fun getMessage(@PathVariable(value = "hash") hash: String): String {
+        val message = MessageHolder.getMessage(hash) ?: throw ResourceNotFoundException()
+        return message
+    }
+
+}
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "This hash has no corresponding message")
+class ResourceNotFoundException : RuntimeException()
